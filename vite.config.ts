e0dbd84512f37fd5aa906fbed9a87b4d27d0d504
Mustafa Buda <mustafabuda@gmail.com>
2026-01-21 22:17:45 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/AutoCare-Oto-Yikama/' : '/',
   server: {
     host: "::",
     port: 8080,
